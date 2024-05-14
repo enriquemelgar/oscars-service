577e88b96766ffbd2d1e75dd47ae0532b75d3e0d
Carlos Aquije Melgar <enrique.a.melgar@gmail.com>
2024-05-13 21:19:13 -0500

    Initial project of oscars service

---
+++ src/main/java/com/aquijemelgar/oscarsservice/OscarsserviceApplication.java
@@ -0,0 +1,13 @@
+package com.aquijemelgar.oscarsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OscarsserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OscarsserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aquijemelgar/oscarsservice/OscarsserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aquijemelgar.oscarsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OscarsserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
